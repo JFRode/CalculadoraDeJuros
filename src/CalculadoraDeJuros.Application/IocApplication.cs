47f988a755249a3900dc630a813d4fed4901818c
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2020-08-01 20:15:35 -0300

    Configurado IocApplication

---
+++ src/CalculadoraDeJuros.Application/IocApplication.cs
@@ -0,0 +1,16 @@
+﻿using CalculadoraDeJuros.Application.Clients;
+using CalculadoraDeJuros.Application.Interfaces.Clients;
+using CalculadoraDeJuros.Application.Interfaces.Services;
+using CalculadoraDeJuros.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CalculadoraDeJuros.Application
+{
+    public static class IocApplication
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services) =>
+            services.AddScoped<ICalculadoraDeJurosService, CalculadoraDeJurosService>()
+                    .AddScoped<ITaxasClient, TaxasClient>()
+                    .AddScoped<IShowMeTheCodeService, ShowMeTheCodeService>();
+    }
+}
