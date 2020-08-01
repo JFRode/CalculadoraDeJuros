3727d5c1a55e8eeb233eae9932177813565bfd08
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2020-08-01 20:07:52 -0300

    Adicionado CalculdadoraDeJurosService

---
+++ src/CalculadoraDeJuros.Application/Interfaces/Services/ICalculadoraDeJurosService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace CalculadoraDeJuros.Application.Interfaces.Services
+{
+    public interface ICalculadoraDeJurosService
+    {
+        Task<decimal> Calcular(decimal valorInicial, int tempo, CancellationToken cancellationToken);
+    }
+}
---
+++ src/CalculadoraDeJuros.Application/Services/CalculadoraDeJurosService.cs
@@ -0,0 +1,25 @@
+﻿using CalculadoraDeJuros.Application.Interfaces.Clients;
+using CalculadoraDeJuros.Application.Interfaces.Services;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CalculadoraDeJuros.Application.Services
+{
+    public class CalculadoraDeJurosService : ICalculadoraDeJurosService
+    {
+        private readonly ITaxasClient _taxasClient;
+
+        public CalculadoraDeJurosService(ITaxasClient taxasClient)
+        {
+            _taxasClient = taxasClient;
+        }
+
+        public async Task<decimal> Calcular(decimal valorInicial, int tempo, CancellationToken cancellationToken)
+        {
+            var calculadora = new Domain.CalculadoraDeJuros.CalculadoraDeJuros();
+            var taxaDeJurosDto = await _taxasClient.GetTaxaDeJuros();
+
+            return await Task.Run(() => calculadora.Calcular(valorInicial, tempo, taxaDeJurosDto.Percentual), cancellationToken);
+        }
+    }
+}
