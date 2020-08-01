e21e788cab09325a47c699db9cafdced6c9389ec
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2020-08-01 19:21:06 -0300

    Adicionado CalculadoraDeJuros

---
+++ src/CalculadoraDeJuros.Domain/CalculadoraDeJuros/CalculadoraDeJuros.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CalculadoraDeJuros.Domain.CalculadoraDeJuros
+{
+    public class CalculadoraDeJuros
+    {
+        public decimal Calcular(decimal valorInicial, int tempo, decimal taxaDeJuros)
+        {
+            var potencia = (decimal)Math.Pow((double)(1 + taxaDeJuros), tempo);
+            var resultado = valorInicial * potencia;
+            return Math.Round(resultado, 2);
+        }
+    }
+}
