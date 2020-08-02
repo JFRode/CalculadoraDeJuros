37dbb745fe9df0b9aa5cc6352ed3d510b0100255
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2020-08-02 10:36:49 -0300

    Adicionado retry com Polly

--- src/CalculadoraDeJuros.API/Startup.cs
+++ src/CalculadoraDeJuros.API/Startup.cs
@@ -1,16 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using CalculadoraDeJuros.Application;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Polly;
+using Polly.Extensions.Http;
+using System;
+using System.Net;
+using System.Net.Http;
 
 namespace CalculadoraDeJuros.API
 {
@@ -29,7 +27,8 @@ namespace CalculadoraDeJuros.API
             services.AddApplicationServices();
 
             services.AddHttpClient("TaxasAPI", client =>
-                client.BaseAddress = new Uri(Configuration["EndpointTaxasApi"]));
+                client.BaseAddress = new Uri(Configuration["EndpointTaxasApi"]))
+                .AddPolicyHandler(GetRetryPolicy());
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -50,5 +49,11 @@ namespace CalculadoraDeJuros.API
                 endpoints.MapControllers();
             });
         }
+
+        private static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy() =>
+            HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+                .WaitAndRetryAsync(6, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
     }
 }
